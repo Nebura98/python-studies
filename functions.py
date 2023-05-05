866fb17f64c3de0d57da5ac78a5f052914abb907
Nebura <hamklieneduprofesional@gmail.com>
2023-05-05 15:31:18 -0600

    Python standart library

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
-#* Unpacking list
-#** Unpacking dictionaries
-def unlimited_arguments(*args):
-    print('')
+# * tells Python to accept an unlimited amount of unnamed arguments and pass them into the function as a tuple
+# ** tells Python to accept and unlimited amount of named arguments and pass them into the function as a dictionary
+def unlimited_arguments(*args, **keyword_args):
+    print(keyword_args)
+    for k, argument in keyword_args.items():
+        print(k, argument)
+
+
+unlimited_arguments(1, 2, 3, 4, name='Nebura', age=29)
